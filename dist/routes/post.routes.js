b4c5b068ddc0f16b3a8e5d7916b29ba656a6019e
Ignacio <belmonteperona@gmail.com>
2020-04-17 00:59:15 +0200

    added get post and delete post

--- dist/routes/post.routes.js
+++ dist/routes/post.routes.js
@@ -18,7 +18,7 @@ const file_system_1 = __importDefault(require("../classes/file-system"));
 const usuario_model_1 = require("../models/usuario.model");
 const fileSystem = new file_system_1.default();
 const postRoutes = express_1.Router();
-//Obtener post
+//Obtener posts
 postRoutes.get('/', (req, res, next) => __awaiter(this, void 0, void 0, function* () {
     try {
         let pagina = Number(req.query.pagina - 1) || 0;
@@ -36,6 +36,23 @@ postRoutes.get('/', (req, res, next) => __awaiter(this, void 0, void 0, function
         });
     }
 }));
+//Obtener Post
+postRoutes.get('/:idPost', (req, res, next) => __awaiter(this, void 0, void 0, function* () {
+    const idPost = req.params.idPost;
+    const post = yield post_model_1.Post.findById(idPost).populate('usuario', '-password').exec();
+    if (post) {
+        return res.json({
+            ok: true,
+            post
+        });
+    }
+    else {
+        return res.json({
+            ok: false,
+            message: 'Id post invalido'
+        });
+    }
+}));
 //Crear post
 postRoutes.post('/', [autenticacion_1.verificaToken], (req, res, next) => {
     const body = req.body;
@@ -50,9 +67,51 @@ postRoutes.post('/', [autenticacion_1.verificaToken], (req, res, next) => {
             post: postDB
         });
     })).catch(err => {
-        res.json(err);
+        res.json({
+            ok: false,
+            err
+        });
     });
 });
+//EliminarPost
+postRoutes.delete('/:idPost', [autenticacion_1.verificaToken], (req, res, next) => __awaiter(this, void 0, void 0, function* () {
+    const idPost = req.params.idPost;
+    const post = yield post_model_1.Post.findById(idPost).exec();
+    if (post) {
+        if (post.usuario == req.usuario._id) {
+            post_model_1.Post.findByIdAndDelete(idPost).exec().then(postBorrado => {
+                if (postBorrado) {
+                    return res.json({
+                        ok: true,
+                        post: postBorrado
+                    });
+                }
+            }).catch(err => {
+                res.json({
+                    ok: false,
+                    message: 'Error durante el borrado',
+                    err
+                });
+            });
+        }
+        else {
+            res.json({
+                ok: true,
+                messgae: 'No eres el usuario que creo el post'
+            });
+        }
+    }
+    else {
+        return res.json({
+            ok: false,
+            message: 'post no encontrado',
+        });
+    }
+}));
+//Actualizar post
+postRoutes.post('/update/:idPost', [autenticacion_1.verificaToken], (req, res, next) => __awaiter(this, void 0, void 0, function* () {
+    //TO-DO Por inmplementar
+}));
 //Subir fichero
 postRoutes.post('/upload', [autenticacion_1.verificaToken], (req, res, next) => __awaiter(this, void 0, void 0, function* () {
     if (!req.files) {
@@ -68,64 +127,73 @@ postRoutes.post('/upload', [autenticacion_1.verificaToken], (req, res, next) => 
             mensaje: 'No se selcciono ningun archivo'
         });
     }
+    //Restriccion solo imagen
     if (!file.mimetype.includes('image')) {
         return res.status(400).json({
             ok: false,
             mensaje: 'Lo que subio no es una imagen'
         });
     }
-    yield fileSystem.guardarImagenTemporal(file, req.usuario._id);
-    res.json({
-        ok: true,
-        file: file
+    fileSystem.guardarImagenTemporal(file, req.usuario._id).then(() => {
+        return res.json({
+            ok: true,
+            file: file
+        });
+    }).catch(err => {
+        return res.json({
+            ok: false,
+            message: 'Error al guardar la imagen',
+            err
+        });
     });
 }));
-postRoutes.get('/imagen/:userid/:img', (req, res, next) => {
+//Obtener imagen post
+postRoutes.get('/imagen/:userid/:img', (req, res, next) => __awaiter(this, void 0, void 0, function* () {
     const userID = req.params.userid;
     const img = req.params.img;
-    if (!usuario_model_1.Usuario.findById(userID)) {
+    const usuario = yield usuario_model_1.Usuario.findById(userID).exec();
+    if (!usuario) {
         return res.status(400).json({
             ok: false,
             mensaje: 'id de usuario incorrecto'
         });
     }
-    const pathImg = fileSystem.getImgUrl(userID, img);
+    const pathImg = fileSystem.getImgUrl(userID, img); // Si no es correcta la imagen devulve imagen por defecto
     return res.sendFile(pathImg);
-});
-// Poner like en post
-postRoutes.post('/like/:postid', [autenticacion_1.verificaToken], (req, res, next) => {
+}));
+// Poner/Quitar like en post
+postRoutes.post('/like/:idPost', [autenticacion_1.verificaToken], (req, res, next) => __awaiter(this, void 0, void 0, function* () {
     const idUsuario = req.usuario._id;
-    const postID = req.params.postid;
-    post_model_1.Post.findById(postID).exec().then((post) => __awaiter(this, void 0, void 0, function* () {
-        if (post) {
-            let existeLike = false;
-            post.likes.forEach(like => {
-                if (like._id == idUsuario) {
-                    existeLike = true;
-                }
-            });
-            if (existeLike) {
-                post.likes.splice(post.likes.indexOf(idUsuario), 1);
+    const idPost = req.params.idPost;
+    const post = yield post_model_1.Post.findById(idPost).exec();
+    if (post) {
+        let existeLike = false;
+        post.likes.forEach(like => {
+            if (like._id == idUsuario) {
+                existeLike = true;
             }
-            else {
-                post.likes.push(idUsuario);
-            }
-            post_model_1.Post.findByIdAndUpdate(postID, post, { new: true }, (err, postDB) => __awaiter(this, void 0, void 0, function* () {
-                if (postDB) {
-                    yield postDB.populate('usuario', '-password').execPopulate();
-                    return res.json({
-                        ok: true,
-                        postDB
-                    });
-                }
-            }));
+        });
+        if (existeLike) {
+            post.likes.splice(post.likes.indexOf(idUsuario), 1);
         }
         else {
-            return res.json({
-                ok: false,
-                error: 'Id post incorrecta'
-            });
+            post.likes.push(idUsuario);
         }
-    }));
-});
+        post_model_1.Post.findByIdAndUpdate(idPost, post, { new: true }, (err, postDB) => __awaiter(this, void 0, void 0, function* () {
+            if (postDB) {
+                yield postDB.populate('usuario', '-password').execPopulate();
+                return res.json({
+                    ok: true,
+                    postDB
+                });
+            }
+        }));
+    }
+    else {
+        return res.json({
+            ok: false,
+            error: 'Id post incorrecta'
+        });
+    }
+}));
 exports.default = postRoutes;
--- routes/post.routes.ts
+++ routes/post.routes.ts
@@ -8,13 +8,12 @@ import { Usuario } from '../models/usuario.model';
 const fileSystem = new FileSystem();
 const postRoutes = Router();
 
-//Obtener post
+//Obtener posts
 postRoutes.get('/', async (req: any, res: Response, next: NextFunction) => {
     try {
         let pagina = Number(req.query.pagina - 1) || 0;
         let saltar = pagina * 10;
         const posts = await Post.find().limit(10).skip(saltar).sort({ _id: -1 }).populate('usuario', '-password').exec();
-
         res.json({
             ok: true,
             posts
@@ -25,18 +24,32 @@ postRoutes.get('/', async (req: any, res: Response, next: NextFunction) => {
             error: 'pagina invalida'
         })
     }
+})
 
+//Obtener Post
+postRoutes.get('/:idPost', async (req: any, res: Response, next: NextFunction) => {
+    const idPost = req.params.idPost;
+    const post = await Post.findById(idPost).populate('usuario', '-password').exec();
+    if (post) {
+        return res.json({
+            ok: true,
+            post
+        })
+    } else {
+        return res.json({
+            ok: false,
+            message: 'Id post invalido'
+        })
+    }
 })
 
 //Crear post
 postRoutes.post('/', [verificaToken], (req: any, res: Response, next: NextFunction) => {
     const body = req.body;
     body.usuario = req.usuario._id
-
     const imagenes = fileSystem.moverImgsEnTempToPost(req.usuario._id);
     console.log(imagenes)
     body.imgs = imagenes;
-
     Post.create(body).then(async postDB => {
         await postDB.populate('usuario', '-password').execPopulate()
         res.json({
@@ -44,12 +57,54 @@ postRoutes.post('/', [verificaToken], (req: any, res: Response, next: NextFuncti
             post: postDB
         })
     }).catch(err => {
-        res.json(err)
+        res.json({
+            ok: false,
+            err
+        })
     });
 })
 
-//Subir fichero
+//EliminarPost
+postRoutes.delete('/:idPost', [verificaToken], async (req: any, res: Response, next: NextFunction) => {
+    const idPost = req.params.idPost;
+    const post = await Post.findById(idPost).exec();
+    if (post) {
+        if (post.usuario == req.usuario._id) {
+            Post.findByIdAndDelete(idPost).exec().then(postBorrado => {
+                if (postBorrado) {
+                    return res.json({
+                        ok: true,
+                        post: postBorrado
+                    })
+                }
+            }).catch(err => {
+                res.json({
+                    ok: false,
+                    message: 'Error durante el borrado',
+                    err
+                });
+            });
+        } else {
+            res.json({
+                ok: true,
+                messgae: 'No eres el usuario que creo el post'
+            })
+        }
+    } else {
+        return res.json({
+            ok: false,
+            message: 'post no encontrado',
+        })
+    }
+});
 
+//Actualizar post
+postRoutes.post('/update/:idPost', [verificaToken], async (req: any, res: Response, next: NextFunction) => {
+    //TO-DO Por inmplementar
+});
+
+
+//Subir fichero
 postRoutes.post('/upload', [verificaToken], async (req: any, res: Response, next: NextFunction) => {
     if (!req.files) {
         return res.status(400).json({
@@ -58,78 +113,80 @@ postRoutes.post('/upload', [verificaToken], async (req: any, res: Response, next
         });
     }
     const file: FileUpload = req.files.imagen
-
     if (!file) {
         return res.status(400).json({
             ok: false,
             mensaje: 'No se selcciono ningun archivo'
         });
     }
-
+    //Restriccion solo imagen
     if (!file.mimetype.includes('image')) {
         return res.status(400).json({
             ok: false,
             mensaje: 'Lo que subio no es una imagen'
         });
     }
-
-    await fileSystem.guardarImagenTemporal(file, req.usuario._id);
-
-    res.json({
-        ok: true,
-        file: file
-    })
+    fileSystem.guardarImagenTemporal(file, req.usuario._id).then(() => {
+        return res.json({
+            ok: true,
+            file: file
+        })
+    }).catch(err => {
+        return res.json({
+            ok: false,
+            message: 'Error al guardar la imagen',
+            err
+        });
+    });
 });
 
-postRoutes.get('/imagen/:userid/:img', (req: any, res: Response, next: NextFunction) => {
+//Obtener imagen post
+postRoutes.get('/imagen/:userid/:img', async (req: any, res: Response, next: NextFunction) => {
     const userID = req.params.userid;
     const img = req.params.img;
-
-    if (!Usuario.findById(userID)) {
+    const usuario = await Usuario.findById(userID).exec()
+    if (!usuario) {
         return res.status(400).json({
             ok: false,
             mensaje: 'id de usuario incorrecto'
         })
     }
-    const pathImg = fileSystem.getImgUrl(userID, img)
-
+    const pathImg = fileSystem.getImgUrl(userID, img)  // Si no es correcta la imagen devulve imagen por defecto
     return res.sendFile(pathImg)
-
 });
 
-// Poner like en post
-postRoutes.post('/like/:postid', [verificaToken], (req: any, res: Response, next: NextFunction) => {
+// Poner/Quitar like en post
+postRoutes.post('/like/:idPost', [verificaToken], async (req: any, res: Response, next: NextFunction) => {
     const idUsuario = req.usuario._id
-    const postID = req.params.postid
-    Post.findById(postID).exec().then(async (post) => {
-        if (post) {
-            let existeLike = false;
-            post.likes.forEach(like=>{
-                if(like._id==idUsuario){
-                    existeLike=true;
-                }
-            })
-            if (existeLike) {
-                post.likes.splice(post.likes.indexOf(idUsuario), 1);
-            } else {
-                post.likes.push(idUsuario);
+    const idPost = req.params.idPost
+    const post = await Post.findById(idPost).exec();
+    if (post) {
+        let existeLike = false;
+        post.likes.forEach(like => {
+            if (like._id == idUsuario) {
+                existeLike = true;
             }
-            Post.findByIdAndUpdate(postID, post, { new: true },async (err, postDB) => {
-                if (postDB) {
-                    await postDB.populate('usuario', '-password').execPopulate()
-                    return res.json({
-                        ok: true,
-                        postDB
-                    })
-                }
-            })
+        })
+        if (existeLike) {
+            post.likes.splice(post.likes.indexOf(idUsuario), 1);
         } else {
-            return res.json({
-                ok: false,
-                error: 'Id post incorrecta'
-            })
+            post.likes.push(idUsuario);
         }
-    })
+        Post.findByIdAndUpdate(idPost, post, { new: true }, async (err, postDB) => {
+            if (postDB) {
+                await postDB.populate('usuario', '-password').execPopulate()
+                return res.json({
+                    ok: true,
+                    postDB
+                })
+            }
+        })
+    } else {
+        return res.json({
+            ok: false,
+            error: 'Id post incorrecta'
+        })
+    }
 })
 
 export default postRoutes;
