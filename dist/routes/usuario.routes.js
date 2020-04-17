80db8f9102365233c794b158420afe7e4356780d
Ignacio <belmonteperona@gmail.com>
2020-04-17 16:51:52 +0200

    added method to follow un follow user

--- dist/routes/usuario.routes.js
+++ dist/routes/usuario.routes.js
@@ -1,4 +1,12 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 }
@@ -41,7 +49,10 @@ userRoutes.post('/login', (req, res) => {
     console.log(body);
     usuario_model_1.Usuario.findOne({ email: body.email }, (err, userDB) => {
         if (err) {
-            throw err;
+            return res.json({
+                ok: false,
+                err
+            });
         }
         if (!userDB) {
             return res.json({
@@ -69,6 +80,48 @@ userRoutes.post('/login', (req, res) => {
         }
     });
 });
+//Follow/Unfollow user
+userRoutes.post('/follow/:idUser', [autenticacion_1.verificaToken], (req, res) => __awaiter(this, void 0, void 0, function* () {
+    const usuarioToFollow = yield usuario_model_1.Usuario.findById(req.params.idUser).exec();
+    const usuario = yield usuario_model_1.Usuario.findById(req.usuario._id).exec();
+    if (!usuarioToFollow || !usuario) {
+        return res.json({
+            ok: false,
+            message: 'No existe el usuario al que quieres seguir'
+        });
+    }
+    console.log(usuario);
+    let isFollowing = false;
+    usuario.following.forEach(usr => {
+        console.log('iiiid', usr.id);
+        console.log('oo', usuarioToFollow._id);
+        if (usr.id == usuarioToFollow._id) {
+            isFollowing = true;
+        }
+    });
+    console.log(isFollowing);
+    if (isFollowing) {
+        usuario.following.splice(usuario.following.indexOf(usuarioToFollow._id), 1);
+        usuarioToFollow.followers.splice(usuario.followers.indexOf(usuario._id), 1);
+    }
+    else {
+        usuario.following.push(usuarioToFollow.id);
+        usuarioToFollow.followers.push(usuario._id);
+    }
+    usuario_model_1.Usuario.findByIdAndUpdate(usuario._id, usuario).exec().then(() => {
+        res.json({
+            ok: true,
+            usuario,
+            usuarioToFollow
+        });
+    }).catch(err => {
+        res.json({
+            ok: false,
+            err
+        });
+    });
+}));
+// actualizar usuario
 userRoutes.post('/update', autenticacion_1.verificaToken, (req, res) => {
     const user = {
         nombre: req.body.nombre || req.usuario.nombre,
@@ -76,8 +129,13 @@ userRoutes.post('/update', autenticacion_1.verificaToken, (req, res) => {
         avatar: req.body.avatar || req.usuario.avatar
     };
     usuario_model_1.Usuario.findByIdAndUpdate(req.usuario._id, user, { new: true }, (err, userDB) => {
-        if (err)
-            throw err;
+        if (err) {
+            return res.json({
+                ok: false,
+                err
+            });
+        }
+        ;
         if (!userDB) {
             return res.json({
                 ok: false,
@@ -97,6 +155,7 @@ userRoutes.post('/update', autenticacion_1.verificaToken, (req, res) => {
         });
     });
 });
+//get usuario from token
 userRoutes.get('/', [autenticacion_1.verificaToken], (req, res) => {
     const usuario = req.usuario;
     res.json({
--- routes/usuario.routes.ts
+++ routes/usuario.routes.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from "express";
-import { Usuario } from '../models/usuario.model';
+import { Usuario, Iusuario } from '../models/usuario.model';
 import bcrypt from 'bcrypt'
 import { Token } from '../classes/token';
 import { verificaToken } from '../middlewares/autenticacion';
@@ -15,7 +15,6 @@ userRoutes.post('/create', (req: Request, res: Response) => {
         avatar: req.body.avatar,
         password: bcrypt.hashSync(req.body.password, 10)
     }
-
     Usuario.create(user).then(userDB => {
         const userToken = Token.getJwtToken({
             _id: userDB.id,
@@ -38,10 +37,13 @@ userRoutes.post('/create', (req: Request, res: Response) => {
 //Login
 userRoutes.post('/login', (req: Request, res: Response) => {
     const body = req.body;
-    console.log( body)
+    console.log(body)
     Usuario.findOne({ email: body.email }, (err, userDB) => {
         if (err) {
-            throw err;
+            return res.json({
+                ok: false,
+                err
+            })
         }
         if (!userDB) {
             return res.json({
@@ -49,7 +51,6 @@ userRoutes.post('/login', (req: Request, res: Response) => {
                 mensaje: 'Usuario/contraseña incorrectos'
             });
         }
-
         if (userDB.compararPassword(body.password)) {
             const userToken = Token.getJwtToken({
                 _id: userDB.id,
@@ -69,19 +70,63 @@ userRoutes.post('/login', (req: Request, res: Response) => {
         }
 
     });
-})
+});
 
+//Follow/Unfollow user
+userRoutes.post('/follow/:idUser', [verificaToken], async (req: any, res: Response) => {
+    const usuarioToFollow = await Usuario.findById(req.params.idUser).exec();
+    const usuario = await Usuario.findById(req.usuario._id).exec();
+    if (!usuarioToFollow || !usuario) {
+        return res.json({
+            ok: false,
+            message: 'No existe el usuario al que quieres seguir'
+        })
+    }
+    console.log(usuario)
+    let isFollowing = false;
+    usuario.following.forEach(usr => {
+        console.log('iiiid', usr.id)
+        console.log('oo', usuarioToFollow._id)
+        if (usr.id == usuarioToFollow._id) {
+            isFollowing = true;
+        }
+    })
+    console.log(isFollowing)
+    if (isFollowing) {
+        usuario.following.splice(usuario.following.indexOf(usuarioToFollow._id), 1);
+        usuarioToFollow.followers.splice(usuario.followers.indexOf(usuario._id), 1);
+    } else {
+        usuario.following.push(usuarioToFollow.id);
+        usuarioToFollow.followers.push(usuario._id);
+    }
+    Usuario.findByIdAndUpdate(usuario._id, usuario).exec().then(() => {
+        res.json({
+            ok: true,
+            usuario,
+            usuarioToFollow
+        })
+    }).catch(err => {
+        res.json({
+            ok: false,
+            err
+        })
+    })
+});
+
+// actualizar usuario
 userRoutes.post('/update', verificaToken, (req: any, res: Response) => {
-
     const user = {
         nombre: req.body.nombre || req.usuario.nombre,
         email: req.body.email || req.usuario.email,
         avatar: req.body.avatar || req.usuario.avatar
     }
-
     Usuario.findByIdAndUpdate(req.usuario._id, user, { new: true }, (err, userDB) => {
-
-        if (err) throw err;
+        if (err) {
+            return res.json({
+                ok: false,
+                err
+            })
+        };
         if (!userDB) {
             return res.json({
                 ok: false,
@@ -97,15 +142,16 @@ userRoutes.post('/update', verificaToken, (req: any, res: Response) => {
         res.json({
             ok: true,
             token: userToken,
-            user:userDB
+            user: userDB
         });
-    })
+    });
 });
 
-userRoutes.get('/',[verificaToken], (req: any, res: Response) => {
+//get usuario from token
+userRoutes.get('/', [verificaToken], (req: any, res: Response) => {
     const usuario = req.usuario;
     res.json({
-        ok:true,
+        ok: true,
         usuario
     });
 });
